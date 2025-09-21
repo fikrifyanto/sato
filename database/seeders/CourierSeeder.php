b4dddd7f3addc3ca239cd454ec27a0730c5fc694
fikrimh <ext_fikri.friyanto@malaysiaairlines.com>
2025-09-22 01:32:48 +0700

    Create a courier seeder

---
+++ database/seeders/CourierSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Courier;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class CourierSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        Courier::create([
+            'name' => 'Fikri',
+            'email' => 'fikri@sato.com',
+            'password' => bcrypt('fikri123'),
+        ]);
+    }
+}
