81f7bd4fdf7dc5863347a982ddc3ba15b023ab15
fikrifyanto <fikrifyanto@gmail.com>
2026-01-07 19:39:18 +0700

    Make order only can view and edit

--- app/Filament/Resources/Orders/Tables/OrdersTable.php
+++ app/Filament/Resources/Orders/Tables/OrdersTable.php
@@ -52,6 +52,7 @@ class OrdersTable
                 //
             ])
             ->recordActions([
+                EditAction::make(),
                 ViewAction::make(),
             ]);
     }
